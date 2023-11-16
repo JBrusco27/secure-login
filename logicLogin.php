77fbf60357d3fcd3b50ed7d20ac42637dc4e2759
Joaquin <joaquinbrusco1@gmail.com>
2023-11-16 16:08:08 -0300

    Second security level

--- logicLogin.php
+++ logicLogin.php
@@ -1,14 +1,31 @@
 <?php
-    $conn = new mysqli('localhost', 'root', '', 'loginBD');
-    if ($conn->connect_error) die("Error de conexión: " . $conn->connect_error);
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "loginBD";
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
     $username = $_POST['username'];
     $password = $_POST['password'];
-    $sql = "SELECT * FROM users WHERE username='$username' AND password='$password'";
-    $result = $conn->query($sql);
 
-    if ($result->num_rows > 0) echo "Inicio de sesión exitoso";
-    else echo "Nombre de usuario o contraseña incorrectos";
-    
-    $conn->close();
+    $sql = "SELECT * FROM users WHERE username = :username AND password = :password";
+    $stmt = $conn->prepare($sql);
+    $stmt->bindParam(':username', $username, PDO::PARAM_STR);
+    $stmt->bindParam(':password', $password, PDO::PARAM_STR);
+    $stmt->execute();
+
+    $result = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    if ($result !== false) {
+        echo "Inicio de sesión exitoso";
+    } else {
+        echo "Nombre de usuario o contraseña incorrectos";
+    }
+
+}
+
 ?>
