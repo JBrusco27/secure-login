1e24902f010c4ca6531aef880ffe245ba22af89a
Joaquin <joaquinbrusco1@gmail.com>
2023-11-16 16:31:44 -0300

    Third security level

--- logicLogin.php
+++ logicLogin.php
@@ -1,31 +1,58 @@
 <?php
+if (!isset($_COOKIE['cantIntentos'])) {
+    $verifExpiry = time() + (1 * 60 * 60); // Calcula la fecha y hora de expiración en 1 hora
+    setcookie('cantIntentos', 1, $verifExpiry, '/');
+    inicioSesion();
+} else {
+    if($_COOKIE['cantIntentos'] <= 3){
+        inicioSesion();
+    }else{
+        echo "Ha realizado demasiados intentos.";
+    }
+}
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
+function inicioSesion()
+{
+    
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "loginBD";
-    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $dbname = "loginBD";
+        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    $username = $_POST['username'];
-    $password = $_POST['password'];
 
-    $sql = "SELECT * FROM users WHERE username = :username AND password = :password";
-    $stmt = $conn->prepare($sql);
-    $stmt->bindParam(':username', $username, PDO::PARAM_STR);
-    $stmt->bindParam(':password', $password, PDO::PARAM_STR);
-    $stmt->execute();
+        $intentos = isset($_COOKIE['cantIntentos']) ? $_COOKIE['cantIntentos'] : 0;
 
-    $result = $stmt->fetch(PDO::FETCH_ASSOC);
+        $username = $_POST['username'];
+        $password = $_POST['password'];
 
-    if ($result !== false) {
-        echo "Inicio de sesión exitoso";
-    } else {
-        echo "Nombre de usuario o contraseña incorrectos";
+        $sql = "SELECT * FROM users WHERE username = :username AND password = :password";
+        $stmt = $conn->prepare($sql);
+        $stmt->bindParam(':username', $username, PDO::PARAM_STR);
+        $stmt->bindParam(':password', $password, PDO::PARAM_STR);
+        $stmt->execute();
+
+        $result = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        
+        if ($result !== false) {
+            echo "Inicio de sesión exitoso";
+            $verifExpiry = time() + (1 * 60 * 60); // Calcula la fecha y hora de expiración en 1 hora
+            setcookie('cantIntentos', 0, $verifExpiry, '/');
+        } else {
+            if ($intentos <= 3) {
+                echo "Nombre de usuario o contraseña incorrectos";
+                $verifExpiry = time() + (1 * 60 * 60); // Calcula la fecha y hora de expiración en 1 hora
+                setcookie('cantIntentos', $intentos + 1, $verifExpiry, '/');
+            } else {
+                echo "Ha realizado demasiados intentos.";
+            }
+        }
+
     }
 
 }
-
 ?>
